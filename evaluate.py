99b5cefeeebbfe47843b8e695218be0c31518651
steakelum <jsteakelum@gmail.com>
2019-07-09 14:12:04 -0400

    restructure nsga code

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,6 @@
 # TODO - firefly - check wei 'b' param - range too wide may be causing numbers to be NaN/inf
 # TODO - improve variable readibility (naming)
 # TODO - re-order functions for readability
-# TODO - add table to illustrations of ranges for params
 # TODO - document variable ranges
 # TODO - check if converged uses endpoints of ranges (perhaps to increase ranges)
 # TODO - explore variants of best ones (bee, bat, pso, etc)
@@ -111,6 +110,94 @@ stage3 = [lambda lst:[model["objective"](x)/model["result"] for x in lst]]*2
 import numpy as np
 import time
 
+
+def search(fn_evals, max_gens, pop_size, p_cross):
+	'''
+	Main routine - creates population of algorithms,
+	runs initial sorting and new population, iteratively
+	runs re-evaluation until convergence
+	'''
+	starting_time = time.time()
+	paramcount = len(max(stage1, key = lambda x: len(x["params"]))["params"])
+
+	algbits = int(np.ceil(np.log2(len(stage1))))
+	fnbits = int(np.ceil(np.log2(len(stage1))) + np.ceil(np.log2(len(stage2))) + np.ceil(np.log2(len(stage3))))
+						# calculate total bits used for picking fn
+	pop = [{"bitstring":random_bitstring(fnbits + nsga_bits_per_param * paramcount)} for i in range(pop_size)]
+
+	print(" > begin initial objectives & sort")
+	calculate_measures(pop, fn_evals)
+	fast_nondominated_sort(pop)
+
+	selected = [better(pop[np.random.randint(pop_size)], pop[np.random.randint(pop_size)]) for i in range(pop_size)]
+	children = reproduce(selected, pop_size, p_cross, algbits)
+
+	print(" > objectives of first child pop")
+	calculate_measures(children, fn_evals)
+	print(" > starting generations")
+
+	for gen in range(max_gens):
+
+		t = {}	
+		for i in [ x[:-3] for x in os.listdir('algorithms') if x[-3:] == '.py']:
+			t[i] = 0
+		for p in pop:
+			m = decode(p["bitstring"])[0].__module__
+			t[m] += 1
+		algs.append(t)
+
+		union = pop + children
+		fronts = fast_nondominated_sort(union)
+		parents = select_parents(fronts, pop_size)
+
+		selected = [better(parents[np.random.randint(pop_size)], parents[np.random.randint(pop_size)]) for i in range(pop_size)]
+		pop = children
+		children = reproduce(selected, pop_size, p_cross, algbits)
+
+		calculate_measures(children, fn_evals)
+		print(f"    > gen = {gen+1} / {max_gens}\tfronts = {len(fronts)}\telapsed={round(time.time()-starting_time, 1)}")
+
+	union = pop + children
+	fronts = fast_nondominated_sort(union)
+	parents = select_parents(fronts, pop_size)
+	return parents
+
+
+def random_bitstring(num_bits):
+	'''
+	Generates a binary string of some length,
+		no limit on size
+	'''
+	return "".join([str(np.random.randint(2)) for i in range(num_bits)]).zfill(num_bits)
+
+
+def calculate_measures(pop, fn_count):
+	'''
+	Given a population member, decodes and evaluates the
+	time and accuracy of its algorithms 
+	'''
+	for index, p in enumerate(pop):                       # find fitness of each member of a population in order to find pareto-fitness
+		alg_1, alg_2, alg_3, params = decode(p["bitstring"])
+
+		runtime = 0
+		errorsum = 0
+		for i in range(fn_count):			# get average of N runs for timing
+			stime = time.time()
+			lst =  alg_1(*params)				# get time of swarm algorithm as well as values for error calc
+			runtime += time.time() - stime
+
+			error_list = alg_3(lst)
+			newerror = sum(error_list)/len(error_list)
+			errorsum += newerror
+
+			p["objectives"] = [runtime/fn_count, errorsum/fn_count]
+											# average runtime and error becomes objectives
+
+		pct = index / (len(pop)-1)	# print out a "progress bar" of the current iteration
+		print(f"\t[{''.join(['▆' if i/16 < pct else '▁' for i in range(16)])}]", end = '\r')
+	print('                              ', end= '\r')
+
+
 def decode(bitstring):
 	'''
 	Takes some bit-pattern (some member of the population),
@@ -142,118 +229,6 @@ def decode(bitstring):
 			params += (prm[0] + prm[1]*diff,)
 
 	return stage1[i1]["algo"], stage2[i2], stage3[i3], params 	# send back algorithms & parameters for timing
-
-def calculate_measures(pop, fn_count):
-	'''
-	Given a population member, decodes and evaluates the
-	time and accuracy of its algorithms 
-	'''
-	for index, p in enumerate(pop):                       # find fitness of each member of a population in order to find pareto-fitness
-		alg_1, alg_2, alg_3, params = decode(p["bitstring"])
-
-		runtime = 0
-		errorsum = 0
-		for i in range(fn_count):			# get average of N runs for timing
-			stime = time.time()
-			lst =  alg_1(*params)				# get time of swarm algorithm as well as values for error calc
-			runtime += time.time() - stime
-
-			error_list = alg_3(lst)
-			newerror = sum(error_list)/len(error_list)
-			errorsum += newerror
-
-			p["objectives"] = [runtime/fn_count, errorsum/fn_count]
-											# average runtime and error becomes objectives
-		pct = index / (len(pop)-1)
-		print(f"\t[{''.join(['▆' if i/16 < pct else '▁' for i in range(16)])}]", end = '\r')
-	print('                              ', end= '\r')
-
-def random_bitstring(num_bits):
-	'''
-	Generates a binary string of some length,
-		no limit on size
-	'''
-	return "".join([str(np.random.randint(2)) for i in range(num_bits)]).zfill(num_bits)
-
-
-def point_mutation(bitstring, rate=None):
-	'''
-	Mutates a bitstring according to some rate,
-	(flips bits at random) - will not change the
-	algorithm-choice bits if not enabled
-	'''
-	fnbits = int(np.ceil(np.log2(len(stage1))))
-
-	if rate == None:					# flip bits at random according to rate
-		rate = 1/len(bitstring)
-	child = ""
-	for i in range(len(bitstring)):
-		bit = bitstring[i]				# only change fn bits if crossbreed is enable
-		if nsga_cross_breed or (i > fnbits):
-			child += str(1-int(bit)) if (np.random.random_sample()<rate) else bit
-		else:
-			child += bit
-	return child
-
-
-def crossover(parent1, parent2, rate):
-	'''
-	Creates hybrid algorithm based on two parents,
-	picks random bits from either, may rarely just
-	use one parent entirely
-	'''
-	if np.random.random_sample() >= rate:
-		return ""+parent1
-
-	if nsga_cross_head_tail:
-		centerpt = np.random.randint(len(parent1))
-		return parent1[:centerpt] + parent2[centerpt:]
-
-	child = ""	# otherwise use random bits method
-	for i in range(len(parent1)):		# get random bits from either parent
-		child += parent1[i] if np.random.random_sample() < 0.5 else parent2[i]
-	return child
-
-
-def reproduce(selected, pop_size, p_cross, bit_count):
-	'''
-	Generates new sub-population based on a selection
-	of an initial population with mutations and crossover
-	of random members - if cross-breed is disabled, 
-	parents will only have same algorithms
-	'''
-	children = []                       # generate new children population based off of parent population
-	for i, p1 in enumerate(selected):   # with crossovers and mutation
-
-		if(not nsga_cross_breed):
-			count = 1
-			while True:					# if no cross breed, get two parents with same algorithms
-				p2 = selected[(i+count)%len(selected)]
-				if p2["bitstring"][:bit_count] == p1["bitstring"][:bit_count]:
-					break
-				count += 1
-		else:
-			p2 = selected[0] if (i == len(selected) - 1) else \
-					selected[i+1] if i%2 == 0 else selected[i-1]
-
-		child = {}						# do crossover and mutations to new child
-		child["bitstring"] = crossover(p1["bitstring"], p2["bitstring"], p_cross)
-		child["bitstring"] = point_mutation(child["bitstring"])
-		children.append(child)
-		if len(children) >= pop_size:
-			break
-	return children
-
-
-def dominates(p1, p2):
-	'''
-	Decides if a candidate p1 is dominant, i.e.
-	all objectives are no worse than the other p2
-	'''
-	for i in range(len(p1["objectives"])): # all traits are no worse than other
-		if p1["objectives"][i] > p2["objectives"][i]:	
-			return False
-	return True
 
 
 def fast_nondominated_sort(pop):
@@ -287,6 +262,86 @@ def fast_nondominated_sort(pop):
 		if not (curr < len(fronts)):
 			break
 	return fronts
+
+
+def dominates(p1, p2):
+	'''
+	Decides if a candidate p1 is dominant, i.e.
+	all objectives are no worse than the other p2
+	'''
+	for i in range(len(p1["objectives"])): # all traits are no worse than other
+		if p1["objectives"][i] > p2["objectives"][i]:	
+			return False
+	return True
+
+
+def point_mutation(bitstring, rate=None):
+	'''
+	Mutates a bitstring according to some rate,
+	(flips bits at random) - will not change the
+	algorithm-choice bits if not enabled
+	'''
+	fnbits = int(np.ceil(np.log2(len(stage1))))
+
+	if rate == None:					# flip bits at random according to rate
+		rate = 1/len(bitstring)
+	child = ""
+	for i in range(len(bitstring)):
+		bit = bitstring[i]				# only change fn bits if crossbreed is enable
+		if nsga_cross_breed or (i > fnbits):
+			child += str(1-int(bit)) if (np.random.random_sample()<rate) else bit
+		else:
+			child += bit
+	return child
+
+
+def reproduce(selected, pop_size, p_cross, bit_count):
+	'''
+	Generates new sub-population based on a selection
+	of an initial population with mutations and crossover
+	of random members - if cross-breed is disabled, 
+	parents will only have same algorithms
+	'''
+	children = []                       # generate new children population based off of parent population
+	for i, p1 in enumerate(selected):   # with crossovers and mutation
+
+		if(not nsga_cross_breed):
+			count = 1
+			while True:					# if no cross breed, get two parents with same algorithms
+				p2 = selected[(i+count)%len(selected)]
+				if p2["bitstring"][:bit_count] == p1["bitstring"][:bit_count]:
+					break
+				count += 1
+		else:
+			p2 = selected[0] if (i == len(selected) - 1) else \
+					selected[i+1] if i%2 == 0 else selected[i-1]
+
+		child = {}						# do crossover and mutations to new child
+		child["bitstring"] = crossover(p1["bitstring"], p2["bitstring"], p_cross)
+		child["bitstring"] = point_mutation(child["bitstring"])
+		children.append(child)
+		if len(children) >= pop_size:
+			break
+	return children
+
+
+def crossover(parent1, parent2, rate):
+	'''
+	Creates hybrid algorithm based on two parents,
+	picks random bits from either, may rarely just
+	use one parent entirely
+	'''
+	if np.random.random_sample() >= rate:
+		return ""+parent1
+
+	if nsga_cross_head_tail:
+		centerpt = np.random.randint(len(parent1))
+		return parent1[:centerpt] + parent2[centerpt:]
+
+	child = ""	# otherwise use random bits method
+	for i in range(len(parent1)):		# get random bits from either parent
+		child += parent1[i] if np.random.random_sample() < 0.5 else parent2[i]
+	return child
 
 
 def calculate_crowding_distance(pop):
@@ -340,57 +395,8 @@ def select_parents(fronts, pop_size):
 		offspring += fronts[last_front][0:remaining]
 	return offspring
 
+# ---------- DONE, RUN SEARCH ALGORITHM -------------------------------------------------------------
 
-def search(fn_evals, max_gens, pop_size, p_cross):
-	'''
-	Main routine - creates population of algorithms,
-	runs initial sorting and new population, iteratively
-	runs re-evaluation until convergence
-	'''
-	starting_time = time.time()
-	paramcount = len(max(stage1, key = lambda x: len(x["params"]))["params"])
-
-	algbits = int(np.ceil(np.log2(len(stage1))))
-	fnbits = int(np.ceil(np.log2(len(stage1))) + np.ceil(np.log2(len(stage2))) + np.ceil(np.log2(len(stage3))))
-						# calculate total bits used for picking fn
-	pop = [{"bitstring":random_bitstring(fnbits + nsga_bits_per_param * paramcount)} for i in range(pop_size)]
-
-	print(" > begin initial objectives & sort")
-	calculate_measures(pop, fn_evals)
-	fast_nondominated_sort(pop)
-
-	selected = [better(pop[np.random.randint(pop_size)], pop[np.random.randint(pop_size)]) for i in range(pop_size)]
-	children = reproduce(selected, pop_size, p_cross, algbits)
-
-	print(" > objectives of first child pop")
-	calculate_measures(children, fn_evals)
-	print(" > starting generations")
-
-	for gen in range(max_gens):
-
-		t = {}	
-		for i in [ x[:-3] for x in os.listdir('algorithms') if x[-3:] == '.py']:
-			t[i] = 0
-		for p in pop:
-			m = decode(p["bitstring"])[0].__module__
-			t[m] += 1
-		algs.append(t)
-
-		union = pop + children
-		fronts = fast_nondominated_sort(union)
-		parents = select_parents(fronts, pop_size)
-
-		selected = [better(parents[np.random.randint(pop_size)], parents[np.random.randint(pop_size)]) for i in range(pop_size)]
-		pop = children
-		children = reproduce(selected, pop_size, p_cross, algbits)
-
-		calculate_measures(children, fn_evals)
-		print(f"    > gen = {gen+1} / {max_gens}\tfronts = {len(fronts)}\telapsed={round(time.time()-starting_time, 1)}")
-
-	union = pop + children
-	fronts = fast_nondominated_sort(union)
-	parents = select_parents(fronts, pop_size)
-	return parents
 
 algs=[]
 pop = search(nsga_fn_evals, nsga_max_gens, nsga_pop_size, nsga_p_cross)
@@ -401,7 +407,6 @@ print("done!\n")
 # ----------- VISUALIZATION -------------------------------------------------------------------------
 
 pop.sort(key = lambda x: x["objectives"][0] * (x["objectives"][1]-1))
-
 
 colors = {
 	'bat':		'#020202',
